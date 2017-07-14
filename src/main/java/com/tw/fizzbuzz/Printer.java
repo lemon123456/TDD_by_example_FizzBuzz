0574d3dc8d25f63b7e320b28a28f88e71e03f4c5
Xiaojuan Xu <xjxu@thoughtworks.com>
2017-07-15 00:06:15 +0800

    Refactor: Printer.fizzbuzz()

--- src/main/java/com/tw/fizzbuzz/Printer.java
+++ src/main/java/com/tw/fizzbuzz/Printer.java
@@ -15,13 +15,14 @@ class Printer {
     }
 
     String fizzbuzz(int number) {
-        String result = "";
-        for (Matcher matcher : matchers) {
-            if (matcher.isMatched(number)) {
-                result += matcher.getText();
-            }
-        }
+        String digitMatcher = getDigitMatcherResult(number);
+        if (!digitMatcher.isEmpty()) return digitMatcher;
 
+        String result = getModMatcherResult(number);
+        return result.isEmpty()? String.valueOf(number) : result;
+    }
+
+    private String getDigitMatcherResult(int number) {
         String[] digits = String.valueOf(number).split("");
         for (String digit : digits) {
             for (DigitMatcher digitMatcher : digitMatchers) {
@@ -30,7 +31,17 @@ class Printer {
                 }
             }
         }
-        return result.isEmpty()? String.valueOf(number) : result;
+        return "";
+    }
+
+    private String getModMatcherResult(int number) {
+        String result = "";
+        for (Matcher matcher : matchers) {
+            if (matcher.isMatched(number)) {
+                result += matcher.getText();
+            }
+        }
+        return result;
     }
 
 }
